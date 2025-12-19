b044e5df9ff6d70c4a8b57c0825c2256465d9037
Abdulrahman Alhemeiri <alhadhrami93@gmail.com>
2025-12-19 09:54:08 +0400

    Implement day 4

---
+++ 2025/Day4/part1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <array>
+
+int main()
+{
+    std::string line;
+    auto const GridSize = 136;
+    std::array<std::array<char, GridSize>, GridSize> grid;
+    int currentInputRow = 0;
+    while (std::getline(std::cin, line))
+    {
+        for (int i = 0; i < GridSize; ++i)
+        {
+            grid[currentInputRow][i] = line[i];
+        }
+        ++currentInputRow;
+    }
+    
+    auto answer = 0;
+    for (int i = 0; i < GridSize; ++i)
+    {
+        for (int j = 0; j < GridSize; ++j)
+        {
+            if (grid[i][j] == '@')
+            {
+                int adjacentRolls = 0;
+                for (int row = i - 1; row <= i + 1; row++)
+                {
+                    for (int column = j - 1; column <= j + 1; column++)
+                    {
+                        if (row >= 0 && row < GridSize && 
+                            column >= 0 && column < GridSize && 
+                            !(row == i && column == j))
+                        {
+                            if (grid[row][column] == '@')
+                            {
+                                ++adjacentRolls;
+                            }
+                        }
+                    }
+                }
+                
+                if (adjacentRolls < 4)
+                {
+                    ++answer;
+                }
+            }
+        }
+    }
+    std::cout << answer << "\n";
+}
